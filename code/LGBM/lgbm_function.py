8ce06a9b8ea27cb0c37ab732f6ab1167e38bb5fa
sunnight9507 <sunnight9507@naver.com>
2021-06-20 03:32:24 +0900

    update

--- code/LGBM/lgbm_function.py
+++ code/LGBM/lgbm_function.py
@@ -5,7 +5,6 @@ import warnings
 import lightgbm as lgb
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import accuracy_score
-from feature_engineering import feature_engineering
 import numpy as np
 import random
 from matplotlib import pylab as plt
@@ -18,22 +17,21 @@ def set_params():
     # 3순위 min_data_in_leaf
     params = {}
     params["boosting_type"] = "dart" # gbdt, dart, goss
-    params["learning_rate"] = 1e-1 # 1e-1, 5e-2, 1e-2, 5e-3, 1e-3
+    params["learning_rate"] = 5e-2 # 1e-1, 5e-2, 1e-2, 5e-3, 1e-3
     params["objective"] = "binary"
     params["metric"] = "auc" # binary_logloss, rmse, huber, auc
-    params["num_iterations"] = 300 # 100
-    params["max_depth"] = 6 # -1
-    params["num_leaves"] = 30 # 31 이상적으로 num_leaves값은 2 ^ (max_depth) 값보다 적거나 같아야 합니다.
-    params["min_data_in_leaf"] = 5000 # 20 100 ~ 1000 수백 또는 수천 개로 정하는 것
-    params["max_bin"] = 32 # 256
-    params["scale_pos_weight"] = 1.1 # 1.1~1.5 data 불균형
-    params["tree_learner"] = "serial" # serial, feature, data, voting
-    params["early_stopping_rounds"] = 100
-    params["bagging_fraction"] = 0.8 # 1.0
-    params["feature_fraction"] = 0.5 # 1.0
-    params["lambda_l1"] = 1e-1 # 0.0
-    params["lambda_l2"] = 1e-1 # 0.0
-    
+    params["num_iterations"] = 100 # 100
+    params["max_depth"] = -1 # -1
+    params["num_leaves"] = 127 # 31 이상적으로 num_leaves값은 2 ^ (max_depth) 값보다 적거나 같아야 합니다.
+    params["min_data_in_leaf"] = 100 # 20 100 ~ 1000 수백 또는 수천 개로 정하는 것
+    params["max_bin"] = 256 # 256
+#     params["scale_pos_weight"] = 0.9 # 1.1~1.5 data 불균형
+#     params["tree_learner"] = "serial" # serial, feature, data, voting
+#     params["early_stopping_rounds"] = 50
+    params["bagging_fraction"] = 0.7 # 1.0
+    params["feature_fraction"] = 0.7 # 1.0
+    params["lambda_l1"] = 0.1 # 0.0
+    params["lambda_l2"] = 0.1 # 0.0   
     
     print("="*30)
     print(params)
@@ -52,7 +50,7 @@ def custom_train_test_split(df, ratio=0.2):
     train_lst = []
     test_lst = []
 
-    max_train_data_len = 0.2*len(df)
+    max_train_data_len = int(ratio*len(df))
     sum_of_train_data = 0
 
     user_ids_lst = []
@@ -69,15 +67,16 @@ def custom_train_test_split(df, ratio=0.2):
         user_ids_lst.append(user_ids)
         
     for user_ids in user_ids_lst:
-        train_lst.append(df[df['userID'].isin(user_ids) == False])
+        train = df[df['userID'].isin(user_ids) == False]        
         test = df[df['userID'].isin(user_ids)]
-        #test데이터셋은 각 유저의 마지막 interaction만 추출
+        train = pd.concat([train, test[test['userID'] == test['userID'].shift(-1)]])
+        train_lst.append(train)
         test_lst.append(test[test['userID'] != test['userID'].shift(-1)])
         
     return train_lst, test_lst
 
 
-def inference(FEATS, model, auc, acc, time):
+def inference(FEATS, model, auc, acc, time, test=False):
     print("="*30)
     print("Start inference")
     print("="*30)
@@ -100,11 +99,9 @@ def inference(FEATS, model, auc, acc, time):
     test_df = pd.concat([df, test_df])
 
     not_test_df = test_df[test_df["answerCode"] != -1]
-    not_test_df = feature_engineering(not_test_df)
     not_test_df["is_test"] = False
 
     test_df = test_df[test_df["answerCode"] == -1]
-    test_df["question_class"] = test_df["assessmentItemID"].apply(lambda x: x[2])
     test_df["is_test"] = True
     
     print("="*30)
@@ -113,19 +110,31 @@ def inference(FEATS, model, auc, acc, time):
     print("="*30)
     print()
     
-    test_df = pd.merge(test_df, not_test_df[["userID", "user_mean"]].drop_duplicates(), on=["userID"], how="inner")
-    test_df = pd.merge(test_df, not_test_df[["question_class", "question_class_mean"]].drop_duplicates(), on=["question_class"], how="inner")
+    not_test_df.sort_values(by=["userID", "Timestamp"], inplace=True)
     
-    def random_answering(data):
-        return 1 if random.random() < data["user_mean"] * data["question_class_mean"] else 0
-
-    test_df["answerCode"] = test_df[["user_mean", "question_class_mean"]].apply(random_answering, axis=1)
-    test_df.drop(["question_class", "user_mean", "question_class_mean"], axis=1, inplace=True)
+    user_mean = not_test_df.groupby(["userID"])["answerCode"].agg(["mean"])
+    user_mean.columns = ["user_mean"]
     
     data = pd.concat([not_test_df, test_df], join="inner")
+    
+    df = pd.merge(data, user_mean, on=["userID"], how="left")
+    
+    df["next_userID"] = df["userID"].shift(-1)
+    
+    def random_answering(data):
+        if data["is_test"]:
+            return 1 if random.random() < 0.5 else 0
+        else:
+            return data["answerCode"]
+
+    df["answercode"] = df.apply(random_answering, axis=1)
 
     # FEATURE ENGINEERING
-    data = feature_engineering(data)
+    df = feature_engineering(df)
+    
+    new_df = df.shift(1)
+    new_df.columns = ["pred_" + i for i in new_df.columns]
+    data = pd.concat([df, new_df], axis=1)
 
     # TEST DATA
     test_df = data[data["is_test"]]
